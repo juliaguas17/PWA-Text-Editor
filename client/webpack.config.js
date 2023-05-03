e090135037313774799001bb33f3af6d5a149c4c
Julianne <juliaguas17@gmail.com>
2023-05-03 18:42:06 -0500

    fixed ToDos in webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -17,13 +15,30 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
-      
-    ],
-
-    module: {
-      rules: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        inject: true
+      }),
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'My Progressive Web App',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+      }),
+      new InjectManifest({
         
+      })
+    ],
+    module: {
+      // TODO: Add CSS loaders and babel to webpack.
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        }, 
       ],
     },
   };
