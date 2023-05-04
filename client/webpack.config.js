b043ea27661c99f960897fbef40dc37b4ed480df
Julianne <juliaguas17@gmail.com>
2023-05-03 19:33:04 -0500

    edits to injections

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,18 +19,19 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './src/index.html',
-        inject: true
       }),
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: 'Just Another Text Editor',
         short_name: 'JATE',
         description: 'Take notes with JavaScript syntax highlighting!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
       }),
       new InjectManifest({
-        
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       })
     ],
     module: {
