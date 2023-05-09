ab1659760d2b796e49e983abfbf595472fa4564c
Julianne <juliaguas17@gmail.com>
2023-05-09 14:00:54 -0500

    added ToDos in webpack.config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,27 +3,72 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack plugin
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Just Another Text Editor'
+      }),
+      // Inject service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // Create manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        start_url: './',
+        publicPath: './',
+        filename: 'manifest.js',
+        theme_color: '#225ca3',
+        background_color: '#225ca3',
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            purpose: 'any maskable',
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      })
     ],
-
     module: {
       rules: [
-        
+        {
+          // CSS loaders
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /^\.m?js$/,
+          exclude: /node_modules/,
+          // Babel loader
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/transform-runtime', '@babel/plugin-proposal-object-rest-spread'],
+            },
+          }
+        }
       ],
     },
   };
